f2d063881fdff24642696f756c9f644d9a468c92
dada01 <LIVE2THEFURTURE@GMAIL.COM>
2024-09-08 00:56:45 +0800

    workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //base: '/0709/',
+  base: '/my-chat-app/',
   build: {
     assetsInlineLimit: 0, // 确保所有资源文件都作为单独的文件被复制
   }
